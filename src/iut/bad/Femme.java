8da840108a4c7b1fec097dfa0a1d83c49b08ecfd
Hendrilalaina <hendrilalaina@gmail.com>
2024-03-20 10:08:21 +0300

    Method main() dans la classe Femme

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -6,4 +6,11 @@ public class Femme extends Humain{
 		this.prenom = prenom;
 		this.age = age;
 	}
+	
+	public static void main(String args[]) {
+		Femme femme = new Femme("Ashley", "Johnsom", 26);
+		Homme homme = new Homme("Bob", "Alvin", 30);
+		
+		femme.ami(homme);
+	}
 }
