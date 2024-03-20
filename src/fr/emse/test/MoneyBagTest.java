61db06ae735698816fad7d847d0682991cbb2fe0
Hendrilalaina <hendrilalaina@gmail.com>
2024-03-20 11:54:44 +0300

    Class MoneyBagTest

---
+++ src/fr/emse/test/MoneyBagTest.java
@@ -0,0 +1,33 @@
+package fr.emse.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MoneyBagTest {
+
+	MoneyBag fMB1, fMB2;
+	Money f12CHF, f14CHF, f7USD, f21USD;
+	@Before
+	public void setUp() throws Exception {
+		f12CHF = new Money(12, "CHF");
+		f14CHF = new Money(14, "CHF");
+		f7USD = new Money(7, "USD");
+		f21USD = new Money(21, "USD");
+		fMB1 = new MoneyBag(f12CHF, f7USD);
+		fMB2 = new MoneyBag(f14CHF, f21USD);
+	}
+
+	@Test
+	public void testEquals() {
+		assertTrue(!fMB1.equals(null));
+		assertEquals(fMB1, fMB1);
+		assertTrue(!fMB1.equals(f12CHF));
+		assertTrue(!f12CHF.equals(fMB1));
+		assertTrue(!fMB1.equals(fMB2));
+	}
+	
+
+}
